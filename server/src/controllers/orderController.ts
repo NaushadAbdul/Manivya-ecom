bf15a6e93303d4e049e535e48dca99ac1812f89a
NaushadAbdul <naushadabdul2006@gmail.com>
2026-08-26 16:21:01 +0530

    Revert "Populate items.product images and enhance product thumbnail resolution in Admin Orders page"
    This reverts commit 4879e15136fc633c0b8ca2b2a2abe1422fa17215.

--- server/src/controllers/orderController.ts
+++ server/src/controllers/orderController.ts
@@ -240,8 +240,7 @@ export const getAllOrdersAdmin = async (req: AuthRequest, res: Response) => {
     if (search) query.orderNumber = { $regex: search as string, $options: 'i' };
 
     const orders = await Order.find(query)
-      .populate('user', 'name email phone photo')
-      .populate('items.product', 'name images image price sellingPrice')
+      .populate('user', 'name email phone')
       .sort({ createdAt: -1 });
 
     return sendSuccess(res, orders, 'All orders fetched for admin');
